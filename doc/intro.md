0c8a67de9d62c825e5a06c462919cfca1e6cf77e
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-25 06:52:22 -0800

    Initial drop of a bare project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to group-match
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/group_match/core_test.clj
@@ -0,0 +1,7 @@
+(ns group-match.core-test
+  (:use clojure.test
+        group-match.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
