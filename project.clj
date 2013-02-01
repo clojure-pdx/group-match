28ab31bfaa8b3b9499df295705883a1c0c024963
Paul deGrandis <paul.degrandis@gmail.com>
2013-02-01 06:32:14 -0800

    Most recent 1.5-RC release for Clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC4"]
                  [org.clojure/core.logic "0.8.0-rc2"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/math.combinatorics "0.0.3"]])
